d5a17bab257f528c881d15986714e79e1eb8885d
Mridul2820 <immridulpanda2820@gmail.com>
2021-12-20 10:23:46 +0530

    login page update

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,6 @@ import { getProviders, signIn } from 'next-auth/react'
 import Head from 'next/head'
 
 const Login = ({ providers }) => {
-    console.log(providers);
     return (
         <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <Head>
@@ -17,14 +16,13 @@ const Login = ({ providers }) => {
             />
 
             {Object.values(providers).map(provider => (
-                <div key={provider.name}>
-                    <button 
-                        className="bg-[#18d860] text-white px-5 py-2"
-                        onClick={() => signIn(provider.id, { callbackUrl: "/"})}
-                    >
-                        Login with {provider.name}
-                    </button>
-                </div>
+                <button 
+                    key={provider.id}
+                    className="bg-[#18d860] text-white px-5 py-2"
+                    onClick={() => signIn(provider.id, { callbackUrl:'/' })}
+                >
+                    Login with {provider.name}
+                </button>
             ))}
         </div>
     )
